523d5a31010bc087cfb113cd35c5e71b04ec04cc
Ali Eskandari <alieskandari3@gmail.com>
2015-08-06 17:46:22 -0400

    Finished user signup
    signup error checking and messages
    signup integration tests
    forced ssl

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test 'invalid signup info' do
+    get signup_path
+
+    # asserts count is same before and after executing the block
+    assert_no_difference 'User.count' do
+      post users_path,  {
+        user: {
+          name:                   "ali",
+          email:                  "ali@invalid",
+          password:               "foo",
+          password_confirmation:  "bar"
+        }
+      }
+    end
+    assert_template 'users/new'
+  end
+
+  test 'valid signup information' do
+    assert_difference 'Users.count', 1 do
+      post_via_redirect users_path, {
+        user: {
+          name:                   "amir esk",
+          email:                  "amie@ail.com",
+          password:               "asdfasdf",
+          password_confirmation:  "asdfasdf"
+        }
+      }
+
+    end
+    assert_template 'users/show'
+  end
+end

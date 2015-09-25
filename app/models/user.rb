368f9432c788487d177c1f66a1a549a471a305b6
Ali Eskandari <alieskandari3@gmail.com>
2015-09-25 09:35:49 -0400

    checkpoint: password reset and account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,10 @@
 class User < ActiveRecord::Base
-	attr_accessor :remember_token
+	attr_accessor :remember_token, :activation_token
 
-	# downcase all emails so that db's w/ case-sensitive indices treat
-	# case-inconsistent email addresses as the same address
-	# 
-	# we use a callback: a method invoked at a particular point in the lifcucle
-	# of an Active Record object. So this call gets made for every User instance
-	# just before it gets saved to the db. It takes in a block
-	before_save { self.email = email.downcase }
+	has_secure_password
+
+	before_save 	:downcase_email
+	before_create :create_activation_digest
 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -17,8 +14,6 @@ class User < ActiveRecord::Base
 						length: 	{ maximum: 255 },
 						format: 	{ with: VALID_EMAIL_REGEX },
 						uniqueness: { case_sensitive: false }
-
-	has_secure_password
 	validates :password, length: { minimum: 6 }, allow_blank: true
 
 	def User.digest(string)
@@ -45,4 +40,24 @@ class User < ActiveRecord::Base
 	def forget
 		update_attribute(:remember_digest, nil)
 	end
+
+	private
+
+		# Converts email to all lowercase
+		def downcase_email	
+			# downcase all emails so that db's w/ case-sensitive indices treat
+			# case-inconsistent email addresses as the same address
+			# 
+			# we use a callback: a method invoked at a particular point in the lifcucle
+			# of an Active Record object. So this call gets made for every User instance
+			# just before it gets saved to the db. It takes in a block
+			self.email = email.downcase
+		end
+
+		# Creates and assigns the activation token and digest
+		def create_activation_digest
+			self.activation_token = User.new_token
+			self.activation_digest = User.digest(activation_token)
+		end
+
 end
